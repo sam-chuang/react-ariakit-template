2463d212608ab8072c665904cc3f2fd5e4f9793a
sam <sam.chuang@rayprosoft.com>
2023-06-29 17:16:14 +0800

    init react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { css } from "@emotion/react"
+import { Routes, Route, Outlet, Link } from "react-router-dom"
 import { Button } from "@ariakit/react"
 //TODO: icon
 // import reactLogo from "./assets/react.svg"
@@ -9,19 +10,42 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Route>
+    </Routes>
+  )
+}
+
+//TODO: page with header & links?
+const Layout = ({}) => {
+  return (
     <>
-      <div className="card">
-        <Button
-          onClick={() => setCount((count) => count + 1)}
-          css={css`
-            color: green;
-          `}
-        >
-          count is {count}
-        </Button>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/nothing-here">Nothing Here</Link>
+            </li>
+          </ul>
+        </nav>
       </div>
+      <Outlet />
     </>
   )
+}
+
+const Home = () => {
+  return "I'm Home!"
+}
+
+const PageNotFound = () => {
+  return "Oops!! Page Not Found"
 }
 
 export default App
